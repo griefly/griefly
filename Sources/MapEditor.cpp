4446927110b08f3c540e4a1ffddd6967e4491253
kremius <anon-df@ya.ru>
2014-06-19 05:54:17 +0400

    bzzz

---
+++ Sources/MapEditor.cpp
@@ -0,0 +1,21 @@
+#include "MapEditor.h"
+
+#include "Screen.h"
+
+void MapEditor::InitWorld()
+{
+}
+
+void MapEditor::Run()
+{
+}
+
+int MapEditor::GetViewerX()
+{
+    return viewer_x_;
+}
+
+int MapEditor::GetViewerY()
+{
+    return viewer_y_;
+}
