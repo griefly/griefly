00ee1a6e652c90b9916a03a01e3785000a8b402e
kremius <anon-df@ya.ru>
2014-07-03 17:50:44 +0400

    Some metadata stuff

--- Sources/Metadata.cpp
+++ Sources/Metadata.cpp
@@ -1,1 +1,14 @@
 #include "Metadata.h"
+
+#include <png.h>
+
+const ImageMetadata::SpriteMetadata& 
+    ImageMetadata::GetSpriteMetadata(const std::string& name)
+{
+    return metadata_[name];
+}
+
+void ImageMetadata::Init(const std::string& name)
+{
+
+}
--- Sources/Metadata.h
+++ Sources/Metadata.h
@@ -1,3 +1,31 @@
 #pragma once
 
-#include <png.h>
+#include <map>
+#include <string>
+#include <vector>
+
+class ImageMetadata
+{
+public:
+    struct SpriteMetadata
+    {
+        SpriteMetadata()
+        {
+            dirs = 1;
+            rewind = false;
+            frames_delay.resize(1);
+            frames_delay[0] = 0;
+            first_frame_pos = 0;
+        }
+
+        size_t dirs;
+        std::vector<size_t> frames_delay;
+        bool rewind;
+
+        size_t first_frame_pos;
+    };
+    void Init(const std::string& file_name);
+    const SpriteMetadata& GetSpriteMetadata(const std::string& name);
+private:
+    std::map<std::string, SpriteMetadata> metadata_;
+};
