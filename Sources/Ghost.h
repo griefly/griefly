adf834db6941f2688cc04496f4751df7ec7918f6
kremius <anon-df@ya.ru>
2014-08-30 20:34:57 +0400

    Ghost vision

--- Sources/Ghost.h
+++ Sources/Ghost.h
@@ -8,5 +8,6 @@ public:
     DECLARE_GET_TYPE_ITEM(Ghost);
     DECLARE_SAVED(Ghost, IMob);
     Ghost();
+    virtual void CalculateVisible(std::list<point>* visible_list) override;
 };
 ADD_TO_TYPELIST(Ghost);
