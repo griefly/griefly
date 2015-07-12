de63570995ade7eb0de88b512475cac8edaaedf5
Alex <sajget@ya.ru>
2015-07-13 02:21:28 +0400

    Closed #86

--- Sources/Ghost.cpp
+++ Sources/Ghost.cpp
@@ -53,10 +53,10 @@ void Ghost::CalculateVisible(std::list<point>* visible_list)
     visible_list->clear();
     point p;
     p.posz = GetZ();
-    int x_low_border = std::max(0, GetX() - sizeHsq + 1);
+    int x_low_border = std::max(0, GetX() - sizeHsq - 1);
     int x_high_border = std::min(GetMapMaster()->GetMapW(), GetX() + sizeHsq);
-    int y_low_border = std::max(0, GetY() - sizeWsq + 1);
-    int y_high_border = std::max(GetMapMaster()->GetMapH(), GetY() + sizeWsq);
+    int y_low_border = std::max(0, GetY() - sizeWsq - 1);
+    int y_high_border = std::min(GetMapMaster()->GetMapH(), GetY() + sizeWsq);
     for (int i = x_low_border; i < x_high_border; ++i)
         for (int j = y_low_border; j < y_high_border; ++j)
         {
