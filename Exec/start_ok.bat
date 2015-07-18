9d3f1b7e0dd45a57113000df50c811dff694cf40
Alex <sajget@ya.ru>
2015-07-18 09:07:34 +0400

    Small changes for release

--- Exec/start_ok.bat
+++ Exec/start_ok.bat
@@ -1,1 +1,1 @@
-START KVEngine.exe -neditor ip=178.49.192.28  mapgen_name=default.gen map_name=atmos_test.map map_x=70 map_y=70 map_z=1 -novsync
+START KVEngine.exe -neditor i1p=178.49.192.28 mapgen_name=default.gen map_x=70 map_y=70 map_z=1 -autogen_reports -auto_connect
