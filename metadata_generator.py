c90eba2e1282fe291d214030254c5a617c609638
Alex <sajget@ya.ru>
2015-07-23 07:24:53 +0400

    Command line options #71

--- metadata_generator.py
+++ metadata_generator.py
@@ -18,14 +18,9 @@ def parse_declare_saved_begin(file, class_and_base, filename):
     class_and_base_array = class_and_base.split(',')
     class_name = class_and_base_array[0].strip()
     base_class_name = class_and_base_array[1].strip()
-    print("Class header is '" + filename + "'")
-    print("Class name is '" + class_name + "'")
-    print("Base class name is '" + base_class_name + "'")
     line = file.next()
     class_type = extract_macros_params(line, "DECLARE_GET_TYPE_ITEM")
-    if class_type:
-        print("Class type '" + class_type + "'")
-    else:
+    if not class_type:
         raise ParseError("Next line after 'DECLARE_SAVED' macros does not contain 'DECLARE_GET_TYPE_ITEM' macros")
 
     variables = []
@@ -40,9 +35,6 @@ def parse_declare_saved_begin(file, class_and_base, filename):
             if result.strip() != class_name:
                 raise ParseError("'ADD_TO_TYPELIST' does not match 'DECLARE_SAVED'")
             break
-
-    print("Class variables:")
-    print(variables)
 
     class_entry = {"class" : class_name,
                    "header" : filename,
@@ -62,13 +54,33 @@ def parse_file(fullpath, filename):
 def path_equal(path1, path2):
     return os.path.abspath(path1) == os.path.abspath(path2)
 
-for directory, directory_names, filenames in os.walk("Sources"):
-    for filename in [f for f in filenames if f.endswith(".h")]:
-        fullpath = os.path.join(directory, filename)
-        if not path_equal(fullpath, "Sources\Macroses.h"):
-            print("Parse file '" + fullpath + "'")
-            parse_file(fullpath, filename)
+def main():
+    dir_name = "Sources"
+    json_name = "metadata.json"
 
-json_data = json.JSONEncoder().encode(metadata)
-with open("metadata.json", "w") as json_file:
-    json_file.write(json_data)
+    if len(sys.argv) > 3:
+        raise ParamsParseError("Too many command line options, use --help for more info")
+
+    if len(sys.argv) == 3:
+        dir_name = sys.argv[1]
+        json_name = sys.argv[2]
+
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "--help":
+            print("Usage: " + sys.argv[0] + " [<source dir> <json output>]")
+            return
+        raise ParamsParseError("Too few command line options, use --help for more info")
+
+    for directory, directory_names, filenames in os.walk(dir_name):
+        for filename in [f for f in filenames if f.endswith(".h")]:
+            fullpath = os.path.join(directory, filename)
+            if not path_equal(fullpath, "Sources\Macroses.h"):
+                #print("Parse file '" + fullpath + "'")
+                parse_file(fullpath, filename)
+
+    json_data = json.JSONEncoder().encode(metadata)
+    with open(json_name, "w") as json_file:
+        json_file.write(json_data)
+
+if __name__ == "__main__":
+    main()
