1930d31e65cce2a94bcada93cf22d7269011f36e
Alex <sajget@ya.ru>
2015-09-01 12:14:23 +0400

    Some interface #117

--- Sources/Network2.cpp
+++ Sources/Network2.cpp
@@ -1,1 +1,18 @@
 #include "Network2.h"
+
+
+Network2 &Network2::GetInstance()
+{
+    static Network2* network = new Network2;
+    return *network;
+}
+
+void Network2::pushReceivedMessage(Message message)
+{
+
+}
+
+Network2::Network2()
+{
+
+}
--- Sources/Network2.h
+++ Sources/Network2.h
@@ -2,6 +2,9 @@
 
 #include <QObject>
 #include <QString>
+#include <QTcpSocket>
+
+#include <QQueue>
 
 struct Message
 {
@@ -13,5 +16,31 @@ class Network2: public QObject
 {
     Q_OBJECT
 public:
-    // such code much technology
+    static Network2& GetInstance();
+
+    bool IsGood();
+
+    void Connect(QString host, int port, QString login, QString password);
+
+    void SendMessage(const Message& message);
+
+    Message RecvMessage();
+
+    void Disconnect();
+
+public slots:
+    void pushReceivedMessage(Message message);
+
+private:
+    QQueue<Message> received_messages_;
+
+    Network2();
+
+    QTcpSocket socket_;
+
+    enum NetworkState
+    {
+        NOT_CONNECTED, CONNECTING, CONNECTED, DISCONNECTED
+    };
+    NetworkState state_;
 };
