1648995d893425d8c6340b0824e0331594d4333e
Alex <sajget@ya.ru>
2015-11-14 00:46:21 +0300

    Class added #138

--- Sources/GraphicRepresentation.h
+++ Sources/GraphicRepresentation.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "View.h"
+
+class GraphicRepresentation
+{
+public:
+    struct Entity
+    {
+        View view;
+        size_t id;
+        std::string name;
+        int posx;
+        int posy;
+    };
+    void Add(const Entity& ent);
+    void Clear();
+};
