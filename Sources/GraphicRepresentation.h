ddca03a24c5006b3c87843e3a0ecdc19fec94f44
Alex <sajget@ya.ru>
2015-11-15 00:32:35 +0300

    #138

--- Sources/GraphicRepresentation.h
+++ Sources/GraphicRepresentation.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <unordered_map>
+
 #include "View.h"
 
 class GraphicRepresentation
@@ -13,6 +15,16 @@ public:
         int posx;
         int posy;
     };
-    void Add(const Entity& ent);
+    void Add(const Entity& ent)
+    {
+
+    }
+
     void Clear();
+private:
+    std::unordered_map<size_t, Entity>* old_frame_;
+    std::unordered_map<size_t, Entity>* new_frame_;
+
+    std::unordered_map<size_t, Entity> first_data_;
+    std::unordered_map<size_t, Entity> seconds_data_;
 };
