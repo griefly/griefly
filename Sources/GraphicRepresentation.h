368b78cbbe57632417b98c01bd7f408db9b3274a
Alex <sajget@ya.ru>
2015-11-16 00:53:39 +0300

    #138

--- Sources/GraphicRepresentation.h
+++ Sources/GraphicRepresentation.h
@@ -1,30 +1,51 @@
 #pragma once
 
 #include <unordered_map>
+#include <vector>
 
 #include "View.h"
 
 class GraphicRepresentation
 {
 public:
+    GraphicRepresentation()
+    {
+        old_frame_ = &first_data_;
+        new_frame_ = &second_data_;
+        is_updated_ = false;
+    }
+
     struct Entity
     {
         View view;
         size_t id;
-        std::string name;
-        int posx;
-        int posy;
+        int pos_x;
+        int pos_y;
+        int vlevel;
+        int dir;
     };
-    void Add(const Entity& ent)
+    void AddToNewFrame(const Entity& ent)
     {
-
+        new_frame_->push_back(ent);
     }
-
-    void Clear();
+    void Swap()
+    {
+        // TODO: mutex
+        // Lock(frames_mutex_);
+        std::swap(old_frame_, new_frame_);
+        is_updated_ = true;
+        new_frame_->clear();
+    }
 private:
-    std::unordered_map<size_t, Entity>* old_frame_;
-    std::unordered_map<size_t, Entity>* new_frame_;
+    bool is_updated_;
 
-    std::unordered_map<size_t, Entity> first_data_;
-    std::unordered_map<size_t, Entity> seconds_data_;
+    typedef std::vector<Entity> DataType;
+
+    DataType* old_frame_;
+    DataType* new_frame_;
+
+    DataType first_data_;
+    DataType second_data_;
+
+    std::unordered_map<size_t, View> views_;
 };
---
+++ Sources/ViewInfo.cpp
@@ -0,0 +1,1 @@
+#include "ViewInfo.h"
