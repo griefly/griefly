ccb885a868d0bc45c79e49eaa8f157e056d5e59d
Alex <sajget@ya.ru>
2015-11-28 04:37:20 +0300

    #138

--- Sources/GraphicRepresentation.cpp
+++ Sources/GraphicRepresentation.cpp
@@ -1,1 +1,20 @@
 #include "GraphicRepresentation.h"
+
+
+void GraphicRepresentation::Process()
+{
+    // TODO: mutex
+    // Lock(mutex_);
+    if (is_updated_)
+    {
+
+    }
+}
+
+void GraphicRepresentation::LoadViewsFromFrame()
+{
+    for (auto it = current_frame_->begin(); it != current_frame_->end(); ++it)
+    {
+        views_[it->id].LoadViewInfo(it->view);
+    }
+}
--- Sources/GraphicRepresentation.h
+++ Sources/GraphicRepresentation.h
@@ -3,21 +3,22 @@
 #include <unordered_map>
 #include <vector>
 
-#include "View.h"
+#include "View2.h"
+#include "ViewInfo.h"
 
 class GraphicRepresentation
 {
 public:
     GraphicRepresentation()
     {
-        old_frame_ = &first_data_;
+        current_frame_ = &first_data_;
         new_frame_ = &second_data_;
         is_updated_ = false;
     }
 
     struct Entity
     {
-        View view;
+        ViewInfo view;
         size_t id;
         int pos_x;
         int pos_y;
@@ -32,20 +33,22 @@ public:
     {
         // TODO: mutex
         // Lock(frames_mutex_);
-        std::swap(old_frame_, new_frame_);
+        std::swap(current_frame_, new_frame_);
         is_updated_ = true;
         new_frame_->clear();
     }
+    void Process();
+    void LoadViewsFromFrame();
 private:
     bool is_updated_;
 
     typedef std::vector<Entity> DataType;
 
-    DataType* old_frame_;
+    DataType* current_frame_;
     DataType* new_frame_;
 
     DataType first_data_;
     DataType second_data_;
 
-    std::unordered_map<size_t, View> views_;
+    std::unordered_map<size_t, View2> views_;
 };
