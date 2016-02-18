3692d4bca893657b24fddf106a5d8090cddc0ed3
Alex <sajget@ya.ru>
2016-02-19 00:58:04 +0300

    Command line parser #206

--- QzipUnpacker/main.cpp
+++ QzipUnpacker/main.cpp
@@ -1,6 +1,10 @@
+#include <QCoreApplication>
 #include <QByteArray>
+#include <QCommandLineParser>
 
 int main(int argc, char* argv[])
 {
+    QCoreApplication app(argc, argv);
+    QCommandLineParser parser;
     return 0;
 }
