295382b130ab136a8a29bc69247e1720cd2dab8f
Alex <sajget@ya.ru>
2016-02-21 02:36:54 +0300

    QCommandLineParser has been removed because of Travis #206

--- QzipUnpacker/main.cpp
+++ QzipUnpacker/main.cpp
@@ -1,10 +1,23 @@
 #include <QCoreApplication>
 #include <QByteArray>
-#include <QCommandLineParser>
+#include <QStringList>
+#include <QTextStream>
+
+inline QTextStream& qStdout()
+{
+    static QTextStream r(stdout);
+    return r;
+}
 
 int main(int argc, char* argv[])
 {
     QCoreApplication app(argc, argv);
-    QCommandLineParser parser;
+
+    QStringList args = app.arguments();
+    if (args.size() != 3)
+    {
+        qStdout() << "Usage: " << args[0] << " [input] [output]";
+    }
+
     return 0;
 }
