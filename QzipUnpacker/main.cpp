c10c6f4cc78b88eaa75434b85ca771968124aa56
Alex <sajget@ya.ru>
2016-02-22 03:26:43 +0300

    Closed #206

--- QzipUnpacker/main.cpp
+++ QzipUnpacker/main.cpp
@@ -2,6 +2,7 @@
 #include <QByteArray>
 #include <QStringList>
 #include <QTextStream>
+#include <QFile>
 
 inline QTextStream& qStdout()
 {
@@ -17,6 +18,28 @@ int main(int argc, char* argv[])
     if (args.size() != 3)
     {
         qStdout() << "Usage: " << args[0] << " [input] [output]";
+        return -1;
+    }
+
+    QFile input(args[1]);
+    if (!input.open(QIODevice::ReadOnly))
+    {
+        qStdout() << "Unable to open file to read";
+        return -1;
+    }
+    QByteArray compressed = input.readAll();
+    QByteArray uncompressed = qUncompress(compressed);
+
+    QFile output(args[2]);
+    if (!output.open(QIODevice::WriteOnly))
+    {
+        qStdout() << "Unable to open file to write";
+        return -1;
+    }
+    if (output.write(uncompressed) == -1)
+    {
+        qStdout() << "Unable to write data to file";
+        return -1;
     }
 
     return 0;
