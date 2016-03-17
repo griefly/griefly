085c7420ed01f7ad76477afebb9a7a24e73bd650
Alex <sajget@ya.ru>
2016-03-17 12:39:41 +0300

    PipeBase #153

--- Sources/core/objects/Pipes.cpp
+++ Sources/core/objects/Pipes.cpp
@@ -1,12 +1,25 @@
 #include "Pipes.h"
 
-Pipe::Pipe(size_t id) : IMovable(id)
+PipeBase::PipeBase(size_t id) : IMovable(id)
 {
     anchored = true;
     v_level = 3;
 
     SetSprite("icons/pipes.dmi");
+    //SetState("intact");
+
+    name = "Please do not create me";
+}
+
+
+Pipe::Pipe(size_t id) : PipeBase(id)
+{
     SetState("intact");
 
     name = "Pipe";
+}
+
+bool Pipe::Connect(Dir dir, id_ptr_on<PipeBase> pipe)
+{
+    return false;
 }
--- Sources/core/objects/Pipes.h
+++ Sources/core/objects/Pipes.h
@@ -1,12 +1,31 @@
 #pragma once
 
 #include "Movable.h"
+#include "../AtmosHolder.h"
 
-class Pipe: public IMovable
+class PipeBase: public IMovable
 {
 public:
-    DECLARE_SAVED(Pipe, IMovable);
+    DECLARE_SAVED(PipeBase, IMovable);
+    DECLARE_GET_TYPE_ITEM(PipeBase);
+    PipeBase(size_t id);
+    virtual bool Connect(Dir dir, id_ptr_on<PipeBase> pipe) { return false; }
+protected:
+    AtmosHolder KV_SAVEBLE(atmos_holder_);
+};
+ADD_TO_TYPELIST(PipeBase);
+
+class Pipe: public PipeBase
+{
+public:
+    DECLARE_SAVED(Pipe, PipeBase);
     DECLARE_GET_TYPE_ITEM(Pipe);
     Pipe(size_t id);
+    virtual bool Connect(Dir dir, id_ptr_on<PipeBase> pipe) override;
+private:
+    id_ptr_on<PipeBase> KV_SAVEBLE(head_);
+    id_ptr_on<PipeBase> KV_SAVEBLE(tail_);
 };
 ADD_TO_TYPELIST(Pipe);
+
+
