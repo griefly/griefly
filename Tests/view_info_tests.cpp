436c4383e73da3218ffec1976b1b4672010fadeb
Alex <sajget@ya.ru>
2016-04-13 00:26:40 +0300

    IsSameSprites tests

--- Tests/view_info_tests.cpp
+++ Tests/view_info_tests.cpp
@@ -4,23 +4,42 @@
 
 TEST(FramesetInfoTest, SettersAndGetters)
 {
-    ViewInfo::FramesetInfo frameset_info_;
-    ASSERT_EQ(frameset_info_.GetSprite(), "");
-    ASSERT_EQ(frameset_info_.GetState(), "");
-    ASSERT_EQ(frameset_info_.GetAngle(), 0);
+    ViewInfo::FramesetInfo frameset_info;
+    ASSERT_EQ(frameset_info.GetSprite(), "");
+    ASSERT_EQ(frameset_info.GetState(), "");
+    ASSERT_EQ(frameset_info.GetAngle(), 0);
 
-    frameset_info_.SetSprite("something");
-    ASSERT_EQ(frameset_info_.GetSprite(), "something");
-    ASSERT_EQ(frameset_info_.GetState(), "");
-    ASSERT_EQ(frameset_info_.GetAngle(), 0);
+    frameset_info.SetSprite("something");
+    ASSERT_EQ(frameset_info.GetSprite(), "something");
+    ASSERT_EQ(frameset_info.GetState(), "");
+    ASSERT_EQ(frameset_info.GetAngle(), 0);
 
-    frameset_info_.SetState("11112");
-    ASSERT_EQ(frameset_info_.GetSprite(), "something");
-    ASSERT_EQ(frameset_info_.GetState(), "11112");
-    ASSERT_EQ(frameset_info_.GetAngle(), 0);
+    frameset_info.SetState("11112");
+    ASSERT_EQ(frameset_info.GetSprite(), "something");
+    ASSERT_EQ(frameset_info.GetState(), "11112");
+    ASSERT_EQ(frameset_info.GetAngle(), 0);
 
-    frameset_info_.SetAngle(42);
-    ASSERT_EQ(frameset_info_.GetSprite(), "something");
-    ASSERT_EQ(frameset_info_.GetState(), "11112");
-    ASSERT_EQ(frameset_info_.GetAngle(), 42);
+    frameset_info.SetAngle(42);
+    ASSERT_EQ(frameset_info.GetSprite(), "something");
+    ASSERT_EQ(frameset_info.GetState(), "11112");
+    ASSERT_EQ(frameset_info.GetAngle(), 42);
+}
+
+TEST(FramesetInfoTest, IsSameSprites)
+{
+    ViewInfo::FramesetInfo frameset_info1;
+    ASSERT_TRUE(ViewInfo::FramesetInfo::IsSameSprites(frameset_info1, frameset_info1));
+
+    frameset_info1.SetSprite("sprite");
+    frameset_info1.SetState("state");
+    frameset_info1.SetAngle(42);
+    ASSERT_TRUE(ViewInfo::FramesetInfo::IsSameSprites(frameset_info1, frameset_info1));
+
+    ViewInfo::FramesetInfo frameset_info2;
+    ASSERT_FALSE(ViewInfo::FramesetInfo::IsSameSprites(frameset_info1, frameset_info2));
+
+    frameset_info2.SetSprite("sprite");
+    frameset_info2.SetState("state");
+    frameset_info2.SetAngle(42);
+    ASSERT_TRUE(ViewInfo::FramesetInfo::IsSameSprites(frameset_info1, frameset_info2));
 }
