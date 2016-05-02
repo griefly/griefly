2f879619a53cda5c4d5274696805120dad7f6439
Alex <sajget@ya.ru>
2016-05-02 13:09:41 +0300

    Branch coverage

--- Sources/Version.cpp
+++ Sources/Version.cpp
@@ -5,10 +5,8 @@
 
 const char* get_game_version()
 {
-    static const char* const VERSION = KV_STR(DEFINED_VERSION);
-    if (VERSION[0] == '\0')
-    {
-        return "v0.0.0-unknown";
-    }
+    static const char* const UNKNOWN = "v0.0.0-unknown";
+    static const char* const VERSION
+            = (KV_STR(DEFINED_VERSION)[0] == '\0') ? UNKNOWN : KV_STR(DEFINED_VERSION);
     return VERSION;
 }
