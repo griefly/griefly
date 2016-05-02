6f04b88096908426e961d9fab9d224f7f5681c6c
Alex <sajget@ya.ru>
2016-05-02 03:45:23 +0300

    Travis fix

--- Sources/Version.cpp
+++ Sources/Version.cpp
@@ -6,5 +6,9 @@
 const char* get_game_version()
 {
     static const char* const VERSION = KV_STR(DEFINED_VERSION);
+    if (VERSION[0] == '\0')
+    {
+        return "v0.0.0-unknown";
+    }
     return VERSION;
 }
