a15895b7f1cd73b957780632efa4d4d8856b9f8d
Alex <sajget@ya.ru>
2016-06-04 00:54:21 +0300

    Save test #250

--- Tests/main_object_tests.cpp
+++ Tests/main_object_tests.cpp
@@ -19,3 +19,13 @@ TEST(MainObject, Types)
    IMainObject object(42);
    ASSERT_EQ(object.T_ITEM(), "main");
 }
+
+TEST(MainObject, Save)
+{
+    {
+        IMainObject object(42);
+        std::stringstream save;
+        object.SaveSelf(save);
+        ASSERT_EQ(save.str(), " main  42  0 ");
+    }
+}
