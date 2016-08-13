5c363024eff1476c975a9b03cf141b6da04f3abc
Alex <sajget@ya.ru>
2016-08-13 17:54:14 +0300

    Files naming fix

---
+++ Sources/core/objects/ProjectileTypes.cpp
@@ -0,0 +1,15 @@
+#include "ProjectileTypes.h"
+
+Bullet::Bullet(size_t id) : Projectile(id)
+{
+    damage_ = 20;
+    SetState("bullet");
+    name = "Bullet";
+}
+
+Laser::Laser(size_t id) : Projectile(id)
+{
+    damage_ = 10;
+    SetState("laser");
+    name = "Laser";
+}
