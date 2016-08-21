453baaa166005d39cd96b3a3f49a3f5da3dbba28
Alex <lagius@grief.ly>
2016-08-21 09:36:59 +0300

    ParamsHolder tests warning has been supressed

--- Tests/params_tests.cpp
+++ Tests/params_tests.cpp
@@ -6,7 +6,18 @@ TEST(Params, Basics)
 {
     ParamsHolder params;
 
-    char* argv[] = {"-var", "var2=text", "var3=3"};
+    const int ARGC = 3;
+    const char* argv_data[ARGC] = {"-var", "var2=text", "var3=3"};
+
+    // argv should be mutable
+    char** argv = new char*[ARGC];
+    for (int i = 0; i < ARGC; ++i)
+    {
+        int length = strlen(argv_data[i]);
+        argv[i] = new char[length];
+        strcpy(argv[i], argv_data[i]);
+    }
+
     params.ParseParams(3, argv);
     ASSERT_TRUE(params.GetParamBool("-var"));
     ASSERT_TRUE(params.GetParamBool("var2"));
@@ -20,4 +31,10 @@ TEST(Params, Basics)
     ASSERT_EQ(params.GetParam<int>("var3"), 3);
     ASSERT_EQ(params.GetParam<std::string>("donot"), "");
     ASSERT_EQ(params.GetParam<int>("donot"), 0);
+
+    for (int i = 0; i < ARGC; ++i)
+    {
+        delete[] argv[i];
+    }
+    delete[] argv;
 }
