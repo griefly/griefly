f2d374291b8908a73847c58bb565a048122bbc42
Alex <sajget@ya.ru>
2016-10-05 09:16:40 +0300

    Sprites for PressureIndicator #197

--- Sources/core/objects/test/PressureIndicator.cpp
+++ Sources/core/objects/test/PressureIndicator.cpp
@@ -2,7 +2,12 @@
 
 PressureIndicator::PressureIndicator(size_t id) : IOnMapObject(id)
 {
-    // Nothing
+    v_level = 11;
+    name = "Pressure indicator";
+    SetSprite("icons/numbers.dmi");
+    SetState("empty");
+    view_.AddOverlay("icons/numbers.dmi", "1").SetShift(-5, 0);
+    view_.AddOverlay("icons/numbers.dmi", "4").SetShift(5, 0);
 }
 
 void PressureIndicator::Process()
