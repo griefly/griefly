737d474f1ea64763c5d021a76543391ded71cfa5
Alex <sajget@ya.ru>
2016-10-30 12:08:15 +0300

    make.sh default build is release without tests

--- make.sh
+++ make.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [[ $# -eq 0 ]] ; then
+    ARGS="-DBUILD_TESTS=OFF -DBUILD_COVER=OFF -DCMAKE_BUILD_TYPE=Release"
+else
+    ARGS="$@"
+fi
+
 mkdir -p build
 cd build
-cmake ../ "$@"
+cmake ../ "$ARGS"
 make && make install
