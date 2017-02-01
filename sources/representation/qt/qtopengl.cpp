fb2bffed48e531c053b886ac45b77ae62afd9136
Alex <sajget@ya.ru>
2017-02-01 21:35:04 +0300

    glOrtho is called with constant params eventually #384

--- sources/representation/qt/qtopengl.cpp
+++ sources/representation/qt/qtopengl.cpp
@@ -67,7 +67,7 @@ void QtOpenGL::initializeGL()
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    glOrtho(0, width(), height(), 0, 0, 1); // TODO: check
+    glOrtho(0, AREA_SIZE_W, AREA_SIZE_H, 0, 0, 1); // TODO: check
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
