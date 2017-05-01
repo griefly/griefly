0d3241f3ae6eb1000e155e8119b31f56190500fe
Alex <sajget@ya.ru>
2017-05-02 00:17:19 +0300

    Line-of-sight class to separate files

--- sources/core/Map.h
+++ sources/core/Map.h
@@ -11,29 +11,7 @@
 
 #include "Interfaces.h"
 
-class LosCalculator
-{
-public:
-    LosCalculator(IMap* map);
-    void Calculate(std::list<PosPoint>* retval, int posx, int posy, int posz = 0);
-private:
-    static int PosToCorner(int pos);
-    static int CornerToPos(int corner);
-    static int Sign(int value);
-    static PosPoint CornerPointToPoint(PosPoint p);
-
-    bool CheckCorner(PosPoint p);
-    bool CheckBorders(int x, int y, int z);
-    bool IsTransparent(PosPoint p);
-    bool BresenX(PosPoint source, PosPoint target);
-    bool BresenY(PosPoint source, PosPoint target);
-    bool RayTrace(PosPoint source, PosPoint target);
-    void MarkTilesOfCornerAsVisible(std::list<PosPoint>* retlist,
-            PosPoint at,
-            PosPoint center,
-            std::vector<char>* visibility);
-    IMap* map_;
-};
+#include "LosCalculator.h"
 
 class Map : public IMap
 {
