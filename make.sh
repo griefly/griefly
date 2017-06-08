f437efaafb04c531567bb0bcee3f6cf0cfdcea0c
bauen1 <j2468h@gmail.com>
2017-06-08 21:53:01 +0200

    Fixed compiling on mac

--- make.sh
+++ make.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Mac:
+#  If you installed qt using brew you need to adjust your enviorment according to `brew info qt`
+#  for example
+# export PATH="/usr/local/opt/qt/bin:$PATH"
+# export LDFLAGS="-L/usr/local/opt/qt/lib"
+# export CPPFLAGS="-I/usr/local/opt/qt/include"
+
 if [[ $# -eq 0 ]] ; then
     ARGS="-DBUILD_TESTS=OFF -DBUILD_COVER=OFF -DCMAKE_BUILD_TYPE=Release"
 else
