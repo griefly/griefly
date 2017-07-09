c3f9ebbdeb99fcf285e9e11adac0eee46c4f2a9d
Alex <sajget@ya.ru>
2017-07-10 00:31:10 +0300

    A little bit more code in the new human interface #433

--- sources/core/objects/mobs/HumanInterface.h
+++ sources/core/objects/mobs/HumanInterface.h
@@ -1,9 +1,35 @@
 #pragma once
 
+#include <QVector>
+
 #include "core/objects/Object.h"
+#include "representation/ViewInfo.h"
+#include "core/SaveableOperators.h"
 
 namespace kv
 {
+class Human;
+class Item;
+
+enum class ItemSlot : quint8
+{
+    DEFAULT,
+    SUIT,
+    HEAD,
+    ANYTHING,
+    FEET,
+    UNIFORM
+};
+
+struct Slot
+{
+    ViewInfo view;
+    IdPtr<Item> item;
+    int posx;
+    int posy;
+    ItemSlot type;
+    QString name;
+};
 
 class HumanInterface2 : public Object
 {
@@ -11,6 +37,9 @@ public:
     DECLARE_SAVEABLE(HumanInterface2, Object);
     REGISTER_CLASS_AS(HumanInterface2);
     HumanInterface2();
+private:
+    IdPtr<Human> KV_SAVEABLE(owner_);
+    QMap<QString, IdPtr<Item>> KV_SAVEABLE(slots_);
 };
 END_DECLARE(HumanInterface2)
 
