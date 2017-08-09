bbbfc47ca91d71d9f957e7fc3b6907487330c43c
fluorescent-net <leavemydog@gmail.com>
2017-08-10 00:49:00 +0300

    Refactor all python utils, update minimum required python version from 2.7 to 3.5, closes #425 (#1)

--- utils/extract_dmi_metadata.py
+++ utils/extract_dmi_metadata.py
@@ -1,53 +1,100 @@
 import sys
-import subprocess
 import json
-import StringIO
 
-if len(sys.argv) < 2:
-    print("Dmi file is missing!")
-    exit(-1)
+from PIL import Image
+from typing import Tuple, Union, List, Dict
 
-metadata = {"info" : {},
-            "states" : []}
 
-def extract_key_value(line):
-    index = line.index('=')
-    key = line[:index].strip(' \t\n\r')
-    value = line[index + 1:].strip(' \t\n\r')  
-    return (key, value) 
-
-print("Extracting metadata from '" + sys.argv[1] + "'...")
-output = subprocess.check_output('identify -format "%[Description]" "' + sys.argv[1] + '"', shell=True)
-if "BEGIN DMI" not in output or "END DMI" not in output:
-    raise ParseError("Dmi metadata is corrupted!")
-
-metadata_io = StringIO.StringIO(output)
-current_object = metadata["info"] 
-for line in metadata_io:
-    if "BEGIN DMI" in line:
-        continue
-    if "END DMI" in line:
-        continue
-    (key, value) = extract_key_value(line)
-    if key == "state":
-        if not value.startswith('"') or not value.endswith('"'):
-            raise ParseError("State '" + value + "' does not have quotes around itself")
-        value = value[1:-1]
-        current_object = {}
-        metadata["states"].append(current_object)
-        current_object["state"] = value;
-        continue;
-    if ',' in value:
-        values = value.split(',')
-        current_object[key] = map(float, values)
-        continue
+def get_filename() -> str:
     try:
-        current_object[key] = int(value)
-    except ValueError:
-        current_object[key] = float(value)
+        return sys.argv[1]
+    except KeyError:
+        raise Exception("Dmi file param is missing!")
 
-json_data = json.JSONEncoder(indent = 4).encode(metadata)
-json_name = sys.argv[1] + ".json"
-with open(json_name, "w") as json_file:
-    json_file.write(json_data)
-print("Metadata was successfully written to '" + json_name + "'")
+
+def get_file(file_name: str) -> Image:
+    try:
+        image = Image.open(file_name)
+    except FileNotFoundError:
+        raise Exception("No such file: {}".format(file_name))
+    except OSError:
+        raise Exception("Improper file format")
+    return image
+
+
+def is_metadata_valid(metadata: dict) -> bool:
+    try:
+        metadata = metadata['Description']
+    except KeyError:
+        return False
+    if "BEGIN DMI" not in metadata or "END DMI" not in metadata:
+        return False
+    return True
+
+
+def format_metadata(image: Image) -> List[str]:
+    print("Extracting metadata from {}...".format(image.filename))
+
+    if is_metadata_valid(image.info):
+        metadata = image.info['Description'].split('\n')
+        metadata = [line.replace('\t', '') for line in metadata]
+        return metadata[1:-2]  # Strip BEGIN DMI/END DMI and empty last line
+    raise Exception("Dmi metadata is corrupted!")
+
+
+def extract_key_value(line: str) -> Tuple[str, Union[List[float], int, float, str]]:
+    key = line.split()[0]
+    value = line.split()[2]
+
+    if key == 'state':
+        value = value[1:-1]  # Strip quote symbols
+        return key, value
+
+    if ',' in value:
+        value = map(float, value.split(','))
+    else:
+        try:
+            value = int(value)
+        except ValueError:
+            value = float(value)
+    return key, value
+
+
+def get_file_info(metadata: List[str]) -> Dict:
+    result = {
+        'info': {},
+        'states': []
+    }
+
+    current_object = result["info"]
+
+    for line in metadata:
+        key, value = extract_key_value(line)
+        if key == "state":
+            current_object = {}
+            result["states"].append(current_object)
+            current_object["state"] = value
+            continue
+
+        if type(value) in [list, int, float]:
+            current_object[key] = value
+            continue
+    return result
+
+
+def metadata_to_json(metadata: Dict) -> str:
+    try:
+        json_data = json.JSONEncoder(indent=4).encode(metadata)
+        return json_data
+    except json.JSONDecodeError:
+        raise Exception("Dmi metadata is corrupted!")
+
+
+file = get_file(get_filename())
+data = get_file_info(format_metadata(file))
+json_file_name = get_filename() + ".json"
+
+with open(json_file_name, "w") as json_file:
+    json_file.write(metadata_to_json(data))
+
+print("Metadata was successfully written to '" + json_file_name + "'")
