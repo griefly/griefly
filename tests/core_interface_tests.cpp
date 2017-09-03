14b59a8eaa2f3b4d0ee9f17f7b798855ea7e8473
Alex <sajget@ya.ru>
2017-09-03 11:46:59 +0300

    FrameData constructors test #403

--- tests/core_interface_tests.cpp
+++ tests/core_interface_tests.cpp
@@ -11,4 +11,16 @@ TEST(CoreInterface, FrameDataAndNestedStructsConstructors)
     EXPECT_EQ(entity.pos_y, 0);
     EXPECT_EQ(entity.vlevel, 0);
     EXPECT_EQ(entity.dir, Dir::SOUTH);
+
+    kv::FrameData::InterfaceUnit unit;
+    EXPECT_EQ(unit.pixel_x, 0);
+    EXPECT_EQ(unit.pixel_y, 0);
+    EXPECT_EQ(unit.shift, 0);
+
+    kv::FrameData::Music music;
+    EXPECT_EQ(music.volume, 0);
+
+    kv::FrameData frame;
+    EXPECT_EQ(frame.camera_pos_x, 0);
+    EXPECT_EQ(frame.camera_pos_y, 0);
 }
