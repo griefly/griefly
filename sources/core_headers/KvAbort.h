a090bd2f07eebf1a3d3eed22c74b2826e039b174
Alex <sajget@ya.ru>
2017-10-27 16:12:34 +0300

    Closed #466

--- sources/core_headers/KvAbort.h
+++ sources/core_headers/KvAbort.h
@@ -1,11 +1,23 @@
 #pragma once
 
+#include <cstdlib>
+
+#include <QDebug>
 #include <QString>
 
 namespace kv
 {
 
-// __gcov_fflush + abort
-[[noreturn]] void Abort(const QString& message);
+#ifdef _BUILD_COVER
+extern "C" void __gcov_flush();
+#endif // _BUILD_COVER
+[[noreturn]] inline void Abort(const QString& message)
+{
+    qDebug() << message;
+#ifdef _BUILD_COVER
+    __gcov_flush();
+#endif // _BUILD_COVER
+    abort();
+}
 
 }
