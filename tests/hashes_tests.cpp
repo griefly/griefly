3273cbdd0deb75a5854306676eb7097d05d2d5bb
Alex <sajget@ya.ru>
2017-11-03 00:50:07 +0300

    ASSERT_EQ to EXPECT_EQ in hashes tests

--- tests/hashes_tests.cpp
+++ tests/hashes_tests.cpp
@@ -7,33 +7,33 @@ using namespace kv;
 
 TEST(HashesTest, IntegerNumber)
 {
-    ASSERT_EQ(Hash(static_cast<unsigned int>(0)), 0);
-    ASSERT_EQ(Hash(1), 1);
-    ASSERT_EQ(Hash(42), 42);
+    EXPECT_EQ(Hash(static_cast<unsigned int>(0)), 0);
+    EXPECT_EQ(Hash(1), 1);
+    EXPECT_EQ(Hash(42), 42);
 
-    ASSERT_NE(Hash(54), 53);
+    EXPECT_NE(Hash(54), 53);
 
     unsigned int test = std::numeric_limits<unsigned int>::max();
-    ASSERT_EQ(Hash(test), test);
+    EXPECT_EQ(Hash(test), test);
 }
 
 TEST(HashesTest, IdPtrOn)
 {
     IdPtr<kv::Object> ptr = 0;
-    ASSERT_EQ(Hash(ptr), 0);
+    EXPECT_EQ(Hash(ptr), 0);
 
     ptr = 131;
-    ASSERT_EQ(Hash(ptr), 131);
+    EXPECT_EQ(Hash(ptr), 131);
 
     ptr = 1000000;
-    ASSERT_EQ(Hash(ptr), 1000000);
+    EXPECT_EQ(Hash(ptr), 1000000);
 }
 
 TEST(HashesTest, String)
 {
     QString s1("hello world");
-    ASSERT_EQ(Hash(s1), 1045060184);
+    EXPECT_EQ(Hash(s1), 1045060184);
 
     s1[4] = '\0';
-    ASSERT_EQ(Hash(s1), 2292011938);
+    EXPECT_EQ(Hash(s1), 2292011938);
 }
