8419f062fb02b232b251c1893d6e4cb25b3945b7
Alex <lagius@grief.ly>
2017-12-15 16:19:01 +0300

    Script for building in docker #462

---
+++ build-in-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Build
+cd /app
+./make.sh -DBUILD_TESTS=ON -DBUILD_COVER=ON -DCMAKE_BUILD_TYPE=Debug
+
+# Tests
+cd /app/exec
+export DISPLAY=:99
+Xvfb $DISPLAY -ac -screen 0 1024x768x8 &
+sleep 3
+./KVEngine --run-tests
+
+# Coverage
+cd /app
+lcov -c -d . -o coverage.info
+lcov --remove coverage.info '/usr/*' --output-file coverage.info
+lcov --remove coverage.info '/opt/*' --output-file coverage.info
+lcov --remove coverage.info 'tests/*' --output-file coverage.info
+lcov --remove coverage.info 'build/*' --output-file coverage.info
+lcov --list coverage.info
