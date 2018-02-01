e84513f73ba49f0b76f71d44816f9fa8827abe12
Alex <sajget@ya.ru>
2018-02-01 13:45:01 +0300

    Some implementation #447

--- sources/core/objects/ObjectProcessor.cpp
+++ sources/core/objects/ObjectProcessor.cpp
@@ -1,1 +1,38 @@
 #include "ObjectProcessor.h"
+
+using namespace kv;
+
+ObjectProcessor::ObjectProcessor()
+{
+    // Nothing
+}
+
+void ObjectProcessor::Add(IdPtr<Object> object)
+{
+    // TODO: better way to check it
+    if (objects_.contains(object))
+    {
+        return;
+    }
+    objects_.append(object);
+}
+
+void ObjectProcessor::RunProcess()
+{
+    const qint32 current_game_tick = GetGameTick();
+    for (auto& object : objects_)
+    {
+        const int object_freq = object->GetFreq();
+
+        if (object_freq == 0)
+        {
+            // TODO: remove object from list
+            return;
+        }
+
+        if ((current_game_tick % object_freq) == 0)
+        {
+            object->Process();
+        }
+    }
+}
--- sources/core/objects/ObjectProcessor.h
+++ sources/core/objects/ObjectProcessor.h
@@ -2,6 +2,10 @@
 
 #include "Object.h"
 
+#include <QVector>
+
+#include "SaveableOperators.h"
+
 namespace kv
 {
 
@@ -11,10 +15,12 @@ public:
     DECLARE_SAVEABLE(ObjectProcessor, Object);
     REGISTER_CLASS_AS(ObjectProcessor);
 
-    // TODO:
-    ObjectProcessor() { }
-    void AfterWorldCreation() override { }
+    ObjectProcessor();
+
+    void Add(IdPtr<Object> object);
+    void RunProcess();
 private:
+    QVector<IdPtr<Object>> KV_SAVEABLE(objects_);
 };
 END_DECLARE(ObjectProcessor);
 
