ef64fe2631ebca18506bf28798dd74d26223fa25
Alex <sajget@ya.ru>
2018-02-08 00:28:19 +0300

    Addition during process test #447

--- sources/core/tests/object_processor_tests.cpp
+++ sources/core/tests/object_processor_tests.cpp
@@ -59,6 +59,7 @@ TEST_F(ObjectProcessorTest, SimpleProcess)
     processor->RunProcess();
     EXPECT_EQ(value1, 0);
 
+    // TODO (?): remove all Add cause SetFreq will call them anyway
     processor->Add(object.Id());
     EXPECT_EQ(processor->HashMembers(), 5);
     processor->Add(object.Id());
@@ -73,4 +74,40 @@ TEST_F(ObjectProcessorTest, SimpleProcess)
     object->SetFreq(0);
     processor->RunProcess();
     EXPECT_EQ(processor->HashMembers(), 2);
+}
+
+TEST_F(ObjectProcessorTest, AddDuringProcess)
+{
+    IdPtr<ObjectProcessor> processor = factory_.CreateImpl(ObjectProcessor::GetTypeStatic());
+
+    IdPtr<TestObject> object1 = factory_.CreateImpl(TestObject::GetTypeStatic());
+    IdPtr<TestObject> object2 = factory_.CreateImpl(TestObject::GetTypeStatic());
+    int value1 = 0;
+    int value2 = 0;
+    object1->SetProcessCallback(
+    [&]()
+    {
+        ++value1;
+        object2->SetFreq(1);
+        processor->Add(object2.Id());
+    });
+    object1->SetFreq(1);
+    processor->Add(object1.Id());
+    object2->SetProcessCallback(
+    [&]()
+    {
+        ++value2;
+    });
+
+    processor->RunProcess();
+    EXPECT_EQ(value1, 1);
+    EXPECT_EQ(value2, 0);
+
+    processor->RunProcess();
+    EXPECT_EQ(value1, 2);
+    EXPECT_EQ(value2, 1);
+
+    processor->RunProcess();
+    EXPECT_EQ(value1, 3);
+    EXPECT_EQ(value2, 2);
 }
