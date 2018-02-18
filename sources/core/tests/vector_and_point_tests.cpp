df68031ae5f2279715298338e81f50088c2ce862
Alex <sajget@ya.ru>
2018-02-18 10:54:17 +0300

    More tests for operators #441

--- sources/core/tests/vector_and_point_tests.cpp
+++ sources/core/tests/vector_and_point_tests.cpp
@@ -36,3 +36,83 @@ TEST(Vector, OperatorPlusEq)
     EXPECT_EQ(vector.y, 3);
     EXPECT_EQ(vector.z, 11);
 }
+
+TEST(Vector, OperatorMultiply)
+{
+    const Vector vector(1, 2, 3);
+
+    {
+        const Vector result = vector * 4;
+        EXPECT_EQ(result.x, 4);
+        EXPECT_EQ(result.y, 8);
+        EXPECT_EQ(result.z, 12);
+    }
+    {
+        const Vector result2 = vector * -1;
+        EXPECT_EQ(result2.x, -1);
+        EXPECT_EQ(result2.y, -2);
+        EXPECT_EQ(result2.z, -3);
+    }
+    {
+        const Vector result3 = 4 * vector;
+        EXPECT_EQ(result3.x, 4);
+        EXPECT_EQ(result3.y, 8);
+        EXPECT_EQ(result3.z, 12);
+    }
+    {
+        const Vector result4 = -1 * vector;
+        EXPECT_EQ(result4.x, -1);
+        EXPECT_EQ(result4.y, -2);
+        EXPECT_EQ(result4.z, -3);
+    }
+}
+
+TEST(Vector, OperatorMultiplyEq)
+{
+    Vector vector(1, 2, 3);
+
+    vector *= 4;
+    EXPECT_EQ(vector.x, 4);
+    EXPECT_EQ(vector.y, 8);
+    EXPECT_EQ(vector.z, 12);
+
+    vector *= -1;
+    EXPECT_EQ(vector.x, -4);
+    EXPECT_EQ(vector.y, -8);
+    EXPECT_EQ(vector.z, -12);
+}
+
+TEST(Vector, OperatorMinus)
+{
+    const Vector vector(1, 1, 1);
+    const Vector vector2(-1, 2, 10);
+
+    {
+        const Vector result = vector - vector2;
+        EXPECT_EQ(result.x, 2);
+        EXPECT_EQ(result.y, -1);
+        EXPECT_EQ(result.z, -9);
+    }
+    {
+        const Vector result = vector2 - vector;
+        EXPECT_EQ(result.x, -2);
+        EXPECT_EQ(result.y, 1);
+        EXPECT_EQ(result.z, 9);
+    }
+}
+
+TEST(Vector, OperatorMinusEq)
+{
+    Vector vector(1, 1, 1);
+    const Vector vector2(-1, 2, 10);
+
+    vector -= vector2;
+    EXPECT_EQ(vector.x, 2);
+    EXPECT_EQ(vector.y, -1);
+    EXPECT_EQ(vector.z, -9);
+
+    vector -= vector2;
+    EXPECT_EQ(vector.x, 3);
+    EXPECT_EQ(vector.y, -3);
+    EXPECT_EQ(vector.z, -19);
+}
