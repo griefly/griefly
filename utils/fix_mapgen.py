2fec4826a660ade3537a18372dfc05c668f621c5
Alex <sajget@ya.ru>
2018-03-27 00:28:48 +0300

    Proper args parser #313

--- utils/fix_mapgen.py
+++ utils/fix_mapgen.py
@@ -2,7 +2,10 @@ import argparse
 
 def main():
     args_parser = argparse.ArgumentParser(description="Fix mapgen file after objects variables changes.")
-    args_parser.add_argument("-a", "--action", help="Possible values: 'rename' & 'remove'.", required=True)
+    args_parser.add_argument("-n", "--rename", help="rename variable.", required=False, nargs=2, metavar=("OLDNAME", "NEWNAME"))
+    args_parser.add_argument("-r", "--remove", help="remove variable.", required=False, nargs=1)
+
+    args = args_parser.parse_args()
 
 if __name__ == "__main__":
     main()
