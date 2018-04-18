50ceb16c9c5c26b5fadcc06117138f83aaa14dc0
Alex <sajget@ya.ru>
2018-04-19 00:03:35 +0300

    Basic visible levels constants have been added #292

--- sources/core/VisibleLevels.h
+++ sources/core/VisibleLevels.h
@@ -3,6 +3,16 @@
 namespace visible_level
 {
 
+const int LOW_TURF = 0;
+const int HIGH_TURF = 2;
+
+const int STRUCTURES = 3;
+
+const int ITEM = 5;
+
+const int LAYING_MOB = 8;
+const int STANDING_MOB = 9;
+
 const int MAX = 20;
 
 }
