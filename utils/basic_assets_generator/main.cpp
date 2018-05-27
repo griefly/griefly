775e0c18e1271706a1674440cc9abf78b909203c
Alex <sajget@ya.ru>
2018-05-27 19:49:26 +0300

    Iterating over metadata #280

--- utils/basic_assets_generator/main.cpp
+++ utils/basic_assets_generator/main.cpp
@@ -2,11 +2,27 @@
 
 #include <CoreInterface.h>
 
+#include <QDebug>
+#include <QJsonObject>
+
+namespace key
+{
+
+const QString TYPENAME("typename");
+
+} // namespace key
+
 int main(int argc, char* argv[])
 {
     QCoreApplication app(argc, argv);
 
     const auto& metadata = kv::GetCoreInstance().GetObjectsMetadata();
+    for (const auto& object_metadata : metadata)
+    {
+        const QJsonObject asset
+            {{key::TYPENAME, object_metadata.name}};
+        qDebug() << asset;
+    }
 
     return 0;
 }
