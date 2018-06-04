3c45f6a2d36925f203a83d4f50582faee00f8e0b
crazzymad777 <aerobus.778@gmail.com>
2018-06-04 17:50:51 +0300

    Pretty view walls

--- sources/core/objects/turfs/MetalWall.cpp
+++ sources/core/objects/turfs/MetalWall.cpp
@@ -4,6 +4,8 @@
 #include "objects/movable/structures/Girder.h"
 #include "Floor.h"
 #include "objects/movable/items/Materials.h"
+#include "objects/mobs/Mob.h"
+#include "objects/mobs/Human.h"
 
 #include "ObjectFactory.h"
 
@@ -17,13 +19,80 @@ MetalWall::MetalWall()
     SetVisibleLevel(visible_level::HIGH_TURF);
 
     SetSprite("icons/walls.dmi");
-    SetState("metal0");
 
     SetName("Metal wall");
+    
+    default_state_="metal";
+    current_state_=0;
+    
+    SetState("metal0");
+}
+void MetalWall::AfterWorldCreation()
+{
+    UpdateState(Dir::ALL);
+}
+void MetalWall::Delete()
+{
+    UpdateNeighborhoodState();
+    MaterialObject::Delete();
+}
+void MetalWall::CheckNeighborhood(Dir dir){
+    if (dir == Dir::ALL)
+    {
+        CheckNeighborhood(Dir::NORTH);
+        CheckNeighborhood(Dir::SOUTH);
+        CheckNeighborhood(Dir::EAST);
+        CheckNeighborhood(Dir::WEST);
+        return;
+    }
+    
+    int bit;
+    
+    switch(dir)
+    {
+        case Dir::NORTH: bit=0; break;
+        case Dir::SOUTH: bit=1; break;
+        case Dir::EAST:  bit=2; break;
+        case Dir::WEST:  bit=3; break;
+        default: return;
+    }
+    
+    if(IdPtr<MetalWall> wall=GetNeighbour(dir)->GetTurf())
+    {
+        current_state_.set(bit, true);  
+        return;
+    }
+    current_state_.set(bit, false);
+}
+
+void MetalWall::UpdateState(Dir dir)
+{
+    CheckNeighborhood(dir);
+    if(dir == Dir::ALL)
+    {
+        UpdateNeighborhoodState();
+    }
+    SetState(default_state_ + QString::number(current_state_.to_ulong()));
+}
+void MetalWall::UpdateNeighborhoodState(Dir dir)
+{
+    if (dir == Dir::ALL)
+    {
+        UpdateNeighborhoodState(Dir::NORTH);
+        UpdateNeighborhoodState(Dir::SOUTH);
+        UpdateNeighborhoodState(Dir::WEST);
+        UpdateNeighborhoodState(Dir::EAST);
+        return;
+    }
+    
+    if(IdPtr<MetalWall> wall=GetNeighbour(dir)->GetTurf())
+    {
+        wall->UpdateState(RevertDir(dir));
+    }
 }
 
 void MetalWall::AttackBy(IdPtr<Item> item)
-{
+{   
     if (IdPtr<Weldingtool> wtool = item)
     {
         if (wtool->IsWorking())
@@ -45,7 +114,7 @@ ReinforcedWall::ReinforcedWall()
     SetVisibleLevel(visible_level::HIGH_TURF);
 
     SetSprite("icons/walls.dmi");
-    SetState("r_wall");
+    SetState("rwall0");
 
     SetName("Reinforced wall");
 }
--- sources/core/objects/turfs/MetalWall.h
+++ sources/core/objects/turfs/MetalWall.h
@@ -1,17 +1,28 @@
 #pragma once
 
 #include "Turf.h"
+#include <bitset>
 
 namespace kv
 {
-
 class MetalWall : public Turf
 {
 public:
     DECLARE_SAVEABLE(MetalWall, Turf);
     REGISTER_CLASS_AS(MetalWall);
     MetalWall();
+
     virtual void AttackBy(IdPtr<Item> item) override;
+    
+    void UpdateState(Dir dir = Dir::ALL);
+    void UpdateNeighborhoodState(Dir dir = Dir::ALL);
+    void CheckNeighborhood(Dir dir = Dir::ALL);
+    
+    virtual void AfterWorldCreation() override;
+    virtual void Delete() override;
+private:
+    QString default_state_;
+    std::bitset<4> current_state_;
 };
 END_DECLARE(MetalWall);
 
